583ffb8cdfc6b2d65a9a762ae0687a5ecaab73fe
vijeth11 <vijeth11@yahoo.co.in>
2018-10-09 11:16:46 +0530

    completed login api

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
+import ast
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Books,Users
 # Create your views here.
@@ -11,12 +15,17 @@ from .models import Books,Users
 #      if(str.isnumeric(row[3])):
 #         foo_instance = Books.objects.create(isbn=row[0],title=row[1],author=row[2],year=int(row[3]))
 #print(os.listdir())
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def login(request):
     if(request.method=='POST'):
-        username=request.POST["username"]
-        password=request.POST["password"]
+        data=ast.literal_eval(request.read().decode("utf-8"))
+
         try:
-            Users.objects.get(name=username,password=password)
-        except:
-            return "user not found"
-    return "hello world"
+            Users.objects.get(username=data['username'],password=data['password'])
+        except Users.DoesNotExist:
+            return HttpResponse("user not found")
+        except KeyError:
+            return HttpResponse("key value error ")
+    return HttpResponse("hello world")
