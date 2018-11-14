72ff58ab067e173b2ce0d23b08ef683df702675c
Ankit Passi <apassi@teksystems.com>
2018-11-14 17:20:08 +0530

    Push Notification

--- Front-End/scripts/app.js
+++ Front-End/scripts/app.js
@@ -1,41 +1,73 @@
-(function() {
-    'use strict';
-  
-    var app = {
-      isLoading: true,
-      visibleCards: {},
-      selectedCities: [],
-      spinner: document.querySelector('.loader'),
-      cardTemplate: document.querySelector('.cardTemplate'),
-      container: document.querySelector('.main'),
-      addDialog: document.querySelector('.dialog-container'),
-      daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    };
-  
-  
-    /*****************************************************************************
-     *
-     * Event listeners for UI elements
-     *
-     ****************************************************************************/
-  
-    function alertMsg()
-    {
-        alert("Message");
-    }
+// if ('serviceWorker' in navigator && 'PushManager' in window) 
+// {
+//   console.log('Service Worker and Push is supported');
 
+//   navigator.serviceWorker.register('scripts/serviceWorker.js')
+//   .then(function(swReg) {
+//     console.log('Service Worker is registered', swReg);
 
-    document.getElementById('butRefresh').addEventListener('click', function() {
-      // Refresh all of the forecasts
-      alertMsg();
+//     swRegistration = swReg;
+//   })
+//   .catch(function(error) {
+//     console.error('Service Worker Error', error);
+//   });
+// } else {
+//   console.warn('Push messaging is not supported');
+//   pushButton.textContent = 'Push Not Supported';
+// }
+const applicationServerPublicKey = 'BADBexvHH5CGvjPuyMlwxhOqcqTG1bvBZgODRaBboG40DrhTyapkWf5uqmddZYv-F3NW8lTAukRpBjE8e8-hTL4';
+
+Notification.requestPermission(function(status) {
+  console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
     });
-  
-    document.getElementById('index_MainButton').addEventListener('click', function() {
-      // Open/show the add new city dialog
-      alertMsg();
+  }
+}
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: 'assets/1.jpg',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {action: 'explore', title: 'Explore this new world',
+            icon: 'images/checkmark.png'},
+          {action: 'close', title: 'Close notification',
+            icon: 'images/xmark.png'},
+        ]
+      };
+      reg.showNotification('Hello world!', options);
     });
-    // TODO add startup code here
-  
-    // TODO add service worker code here
-  })();
-  
+  }
+}
+
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
+
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    clients.openWindow('http://www.example.com');
+    notification.close();
+  }
+});
